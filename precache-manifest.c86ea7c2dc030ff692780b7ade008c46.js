f8b28c4aef833b12c9a8f3f368c04ed58a661d58
iamsomraj <iamsomraj@gmail.com>
2020-01-21 18:04:41 +0530

    Updates

---
+++ precache-manifest.c86ea7c2dc030ff692780b7ade008c46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f264f4384b9964aecdecd2e5d4b26b02",
+    "url": "/React-App-Approval-Card-with-Children/index.html"
+  },
+  {
+    "revision": "ec78251ca253c8df2809",
+    "url": "/React-App-Approval-Card-with-Children/static/js/2.f80285ce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-App-Approval-Card-with-Children/static/js/2.f80285ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5e61268a9e7e2e66b01",
+    "url": "/React-App-Approval-Card-with-Children/static/js/main.64f6d4d4.chunk.js"
+  },
+  {
+    "revision": "7f70604bc7445323b5be",
+    "url": "/React-App-Approval-Card-with-Children/static/js/runtime-main.ed12c123.js"
+  }
+]);
